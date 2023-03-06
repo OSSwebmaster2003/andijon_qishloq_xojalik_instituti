7f565fd4838c9eb1de034a2137478451fcce3867
OSSwebmaster2003 <sodikovotabeksobirjonovich1509@gmail.com>
2023-03-06 04:59:56 -0800

    flats page finished

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RoomOrders from "./pages/RoomOrders/RoomOrders";
 import Books from "./pages/Books/Books";
 import Header from "./common/Header/Header";
 import Footer from "./common/Footer/Footer";
+import RoomList from "./pages/RoomList/RoomList";
 
 function App(props) {
   return (
@@ -14,6 +15,7 @@ function App(props) {
         <Route path="/" element={<Home />} />
         <Route path="/room_ordering" element={<RoomOrders />} />
         <Route path="/library" element={<Books />} />
+        <Route path="/house_renting" element={<RoomList />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/RoomList/RoomList.jsx
+++ src/pages/RoomList/RoomList.jsx
@@ -1,8 +1,54 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Heading from "../../components/Header/Heading";
 import "./roomList.scss";
 
 function RoomList(props) {
-  return <div></div>;
+  const { flats } = useSelector((state) => state.data);
+  return (
+    <div className="room_list_page">
+      <div className="room_list_wrapper">
+        <div className="room_list_header">
+          <Heading title="Xonadonlar ro‘yxati" />
+        </div>
+        <div className="room_list_body">
+          {flats.map((item) => (
+            <div className="flats_card" key={item.id}>
+              <div className="card_img">
+                <img src={item.img} alt="" />
+              </div>
+              <div className="card_body">
+                <h1>{item.title}</h1>
+                <div className="card_details">
+                  <p>
+                    <span>Uy Egasi: </span> {item.owner}
+                  </p>
+                  <p>
+                    <span>Manzil: </span> {item.location}
+                  </p>
+                  <p>
+                    <span>Talaba: </span> {item.requirement}
+                  </p>
+                  <p>
+                    <span>Bo'sh joylar soni: </span> 2ta
+                  </p>
+                  <p>
+                    <span>Sharoitlar: </span> {item.condition}
+                  </p>
+                </div>
+                <div className="card_actions">
+                  <h2>Narxi : {item.cost}</h2>
+                  <p>
+                    <span>Telefon raqami: </span> {item.call_number}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default RoomList;
